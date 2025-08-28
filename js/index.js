bd34cadb171900387e54afc3095ba95c36f333eb
unknown <alamgirhossaindrubo@gmail.com>
2025-08-29 02:05:16 +0600

    Call history dynamically added and Heart icon

--- js/index.js
+++ js/index.js
@@ -1,24 +1,45 @@
-document.getElementById('call-btn')
-    .addEventListener('click', function () {
-        const coins = parseInt(document.getElementById('total-coin'));
-
-        if (coins < 20) {
-            alert('You do not able to make a call.Minimum 20 coins needed to make a call.');
-            return;
-        } else {
-
-        }
-    });
-
 function makeCall(name, number) {
-    var coins = parseInt(document.getElementById('total-coin'));
+    var coins = parseInt(document.getElementById('total-coin').innerText);
+    console.log('Hello from makeCall, coins =', coins);
 
     if (coins < 20) {
-        alert('You do not able to make a call.Minimum 20 coins needed to make a call.');
+        alert('You do not have enough coins. Minimum 20 needed.');
         return;
     } else {
         var s = `Calling ${name} ${number}...`;
-        coins -= 20;
+        const newCoins = coins - 20;
+        document.getElementById('total-coin').innerText = newCoins;
+
+
+        const historyContainer = document.getElementById("callHistory");
+
+        const now = new Date();
+        const timeString = now.toLocaleTimeString();
+
+        const history = document.createElement("div");
+        history.className =  "flex justify-between items-center p-5 rounded-xl bg-[#E5DDDD] text-black my-1";
+
+        const left = document.createElement("div");
+        left.innerHTML = `<div>${name}</div><div>${number}</div>`;
+
+        const right = document.createElement("div");
+        right.textContent = timeString;
+
+        history.appendChild(left);
+        history.appendChild(right);
+        historyContainer.prepend(history);
+
         alert(s);
     }
 }
+
+
+
+document.querySelectorAll(".fa-heart").forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        const heartCount = document.getElementById('total-love').innerText;
+        const newHeartCount = parseInt(heartCount) + 1;
+        document.getElementById('total-love').innerText = newHeartCount;
+        console.log('Heart clicked', newHeartCount);
+    });
+});
